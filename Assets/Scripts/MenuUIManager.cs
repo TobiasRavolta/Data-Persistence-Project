9af1e07b02e6b91f044fbafaa8bc91c602076a6a
TobiasRavolta <tobias@diegasslers.de>
2022-07-09 08:10:37 +0200

    Basic Data Persistence
    High Score now gets changed regardless of new scene or session

--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -13,9 +14,19 @@ public class MenuUIManager : MonoBehaviour
     public GameObject menu1;
     public GameObject menu2;
 
+    public Text nameEntryOutputText;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        if (nameEntryOutputText.text.ToCharArray().Length > 1)
+        {
+            MainManager.playerName = nameEntryOutputText.text;
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Error: Your name needs to be more than 1 character long");
+        }
     }
 
     public void Exit()
